cee9117d9d6bfb677def1a032fdaba9cccd3f111
Ankit Antony <x00193216@mytudublin.ie>
2023-08-07 13:18:00 +0100

    commit with all files

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Reference to the current subscription.  Used when creating role assignments
+data "azurerm_subscription" "current" {}
+
+resource "random_pet" "rg" {
+  length = 1
+  prefix = var.name
+}
+
+# The main resource group for this deployment
+resource "azurerm_resource_group" "default" {
+  name     = "${random_pet.rg.id}-${var.environment}-rg"
+  location = var.location
+}
